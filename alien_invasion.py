af06e78794f6d363c8ea3b486bc0c0d9a9a866b6
Danyuan Wang <danyuanwang@hotmail.com>
2019-01-09 17:44:16 -0800

    second commit'

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,22 +1,31 @@
-import sys
-
 import pygame
-
+from settings import Settings
 from ship import Ship
+from alien import Alien
+
+import game_functions as gf
+from pygame.sprite import Group
+
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (90, 180, 200)
-    ship = Ship(screen)
+
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    print (bullets)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(bg_color)
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update()
         ship.blitme()
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        bullets.update()
+        #get rid of bullets that have disappeared
+        gf.update_bullets(bullets)
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
 run_game()
